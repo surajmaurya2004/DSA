72a277183d22915976b2d43a049bee7de4c5498c
Suraj Maurya <152968969+surajmaurya2004@users.noreply.github.com>
2026-01-31 00:28:25 +0530

    Time: 157 ms (81.44%) | Memory: 119 MB (60.28%) - LeetSync

---
+++ 1866-restore-the-array-from-adjacent-pairs/restore-the-array-from-adjacent-pairs.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    vector<int> result;
+    void dfs(int u, int prev,unordered_map<int, vector<int>> &adj){
+        result.push_back(u);
+        for(int &v : adj[u]){
+            if(v != prev){
+                dfs(v,u,adj);
+            }
+        }
+    }
+    
+    vector<int> restoreArray(vector<vector<int>>& adjacentPairs) {
+        unordered_map<int, vector<int>> adj;
+        for(vector<int> &vec : adjacentPairs){
+            int u = vec[0];
+            int v = vec[1];
+            adj[u].push_back(v);
+            adj[v].push_back(u);
+        }
+        int startPoint = -1;
+        for(auto &it : adj){
+            if(it.second.size() == 1){
+                startPoint = it.first;
+                break;
+            }
+        }
+        dfs(startPoint, INT_MIN, adj);
+        return result;
+
+    }
+};
